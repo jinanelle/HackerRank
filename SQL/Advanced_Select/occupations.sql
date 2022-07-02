0ddcab73957523d8451981bead661e5bacd19d95
Jinan <35148534+jinanelle@users.noreply.github.com>
2022-07-02 16:38:11 +0200

    Advanced Select

--- SQL/Advanced_Select/occupations.sql
+++ SQL/Advanced_Select/occupations.sql
@@ -3,6 +3,11 @@ Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabeti
 
 Note: Print NULL when there are no more names corresponding to an occupation.
 
+Input Format
+- OCCUPATIONS Table
+    Name: String
+    Occupation: String
+
 Sample Output
 Jenny    Ashley     Meera  Jane
 Samantha Christeen  Priya  Julia
@@ -10,24 +15,24 @@ NULL     Ketty      NULL   Maria
 
 Explanation
 The first column is an alphabetically ordered list of Doctor names.
-The second column is an alphabetically ordered list of Professor names.
-The third column is an alphabetically ordered list of Singer names.
-The fourth column is an alphabetically ordered list of Actor names.
-The empty cell data for columns with less than the maximum number of names per occupation (in this case, the Professor and Actor columns) are filled with NULL values.
 
 Pivot table : Rotate rows to columns
 */
 
---'Doctor', 'Professor', 'Singer', 'Actor'
-/*
-SELECT Occupation, COUNT(*)
-FROM OCCUPATIONS
-GROUP BY 1
-ORDER BY 2
---
-SELECT COUNT(*)
-FROM OCCUPATIONS
-GROUP BY Occupation 
-ORDER BY 1 DESC
-LIMIT 1
-*/
+WITH tb AS (
+    SELECT 
+        CASE WHEN Occupation = 'Doctor' THEN @rD := @rD +1
+             WHEN Occupation = 'Professor' THEN @rP := @rP + 1
+             WHEN Occupation = 'Singer' THEN @rS := @rS + 1 
+             WHEN Occupation = 'Actor' THEN @rA := @rA + 1 
+             END row_idx,
+        CASE WHEN Occupation = 'Doctor' THEN Name END Doctor,
+        CASE WHEN Occupation = 'Professor' THEN Name END Professor,
+        CASE WHEN Occupation = 'Singer' THEN Name END Singer,
+        CASE WHEN Occupation = 'Actor' THEN Name END Actor
+    FROM OCCUPATIONS,
+        (SELECT @rD:=0, @rP:=0, @rS:=0, @rA:=0) r
+    ORDER BY Name)
+SELECT MAX(Doctor), MAX(Professor), MAX(Singer), MAX(Actor)
+FROM tb
+GROUP BY row_idx
