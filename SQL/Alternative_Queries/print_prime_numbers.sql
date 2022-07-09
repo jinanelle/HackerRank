33b03f2cd0719b897f602f29b0f6bcb629ee4b8a
Jinan <35148534+jinanelle@users.noreply.github.com>
2022-07-09 20:59:53 +0200

    Alternative Queries

--- SQL/Alternative_Queries/print_prime_numbers.sql
+++ SQL/Alternative_Queries/print_prime_numbers.sql
@@ -7,11 +7,20 @@ For example, the output for all prime numbers <= 10  would be:
 
 */
 
-SET @row_num:=0;
-WITH tb AS (
-    SELECT @row_num:=@row_num+1 AS row_idx
-    FROM INFORMATION_SCHEMA.TABLES t1,INFORMATION_SCHEMA.TABLES t2
-    LIMIT 1000 )
-SELECT row_idx 
-FROM tb
-ORDER BY row_idx;
+SET @row_num:=1;
+WITH 
+numbers AS (
+    SELECT @row_num:=@row_num+1 AS val
+    FROM INFORMATION_SCHEMA.TABLES t1, INFORMATION_SCHEMA.TABLES t2
+    LIMIT 999 ),
+numbers_reminders AS (
+    SELECT n1.val, 
+    (SELECT COUNT(n2.val)
+    FROM numbers n2
+    WHERE n2.val < ((n1.val / 2) + 1) AND  MOD(n1.val, n2.val) = 0) reminders
+    FROM numbers n1
+    )
+SELECT GROUP_CONCAT(val SEPARATOR '&')
+FROM numbers_reminders
+WHERE reminders = 0
+ORDER BY 1
